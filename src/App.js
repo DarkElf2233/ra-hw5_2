b68170a3c2c15472503f573cbaa6675b0466b94e
DarkElf2233 <fd.melnikoff@gmail.com>
2024-08-04 17:51:38 +0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { News } from './components/News'
+import { Banner } from './components/Banner'
+import { Advert } from './components/Advert'
+import { SearchGroup } from './components/SearchGroup'
+import { TopEvents } from './components/TopEvents'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">App</div>
   );
 }
 
